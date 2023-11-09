deb54c69a119b250e4aeec8902ed4b9f16245110
hong-jiyoung <ghdwldud1119@gmail.com>
2023-11-09 15:00:01 +0900

    Update Purchase.java

--- deliver/src/main/java/com/tp/deliver/model/Purchase.java
+++ deliver/src/main/java/com/tp/deliver/model/Purchase.java
@@ -1,5 +1,44 @@
 package com.tp.deliver.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="purchase")
 public class Purchase {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 
+	private long id;
+	
+	@ManyToOne
+	private Deliver delivers;
+	
+	@Column(name="login_id")
+			private String loginId;
+	
+	private int quantity;
+	
+	
+	public Purchase() {
+		super();
+	}
+
+	public Purchase(long id, Deliver delivers, String loginId, int quantity) {
+		super();
+		this.id = id;
+		this.delivers = delivers;
+		this.loginId = loginId;
+		this.quantity = quantity;
+	}
+
+	
 
 }
